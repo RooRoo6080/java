1e5c8a3b68e3eb3ea1be68305d2537d1d2aa3f23
RooRoo6080 <reuelriju@gmail.com>
2021-06-18 13:32:48 -0700

    first commit

---
+++ src/leetcode/Sqrt_x.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class Sqrt_x {
+    public int mySqrt(int x) {
+        long r = x;
+        while (r*r > x) r = (r + x/r) / 2;
+        return (int) r;
+    }
+
+    public static void main(String[] args) {
+        Sqrt_x sqx = new Sqrt_x();
+        System.out.println(sqx.mySqrt(43));
+    }
+}
