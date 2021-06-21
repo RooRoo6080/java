2eb04bfcb215436994cda21e501c988186520e7b
RooRoo6080 <reuelriju@gmail.com>
2021-06-21 10:45:42 -0700

    third commit

--- src/leetcode/MergeSortedArray.java
+++ src/leetcode/MergeSortedArray.java
@@ -4,33 +4,16 @@ import java.util.Arrays;
 
 public class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
+        int pos = m + n - 1;
         m--;
         n--;
-        int pos = m + n - 1;
 
         while (m >= 0 && n >= 0) {
-            //if (nums1[m] > nums2[n]) nums1[pos--] = nums1[m--];
-            //else nums1[pos--] = nums2[n--];
-            nums1[pos--] = (nums1[m] > nums2[n]) ? nums1[m--] : nums2[n--];
+            if (nums1[m] > nums2[n]) nums1[pos--] = nums1[m--];
+            else nums1[pos--] = nums2[n--];
         }
 
         while (n >= 0) nums1[pos--] = nums2[n--];
-
-
-        /*
-        int tail1 = m - 1, tail2 = n - 1, finished = m + n - 1;
-        while (tail1 >= 0 && tail2 >= 0) {
-            nums1[finished--] = (nums1[tail1] > nums2[tail2]) ?
-                    nums1[tail1--] : nums2[tail2--];
-        }
-
-        while (tail2 >= 0) {
-            nums1[finished--] = nums2[tail2--];
-        }
-
-         */
-
         System.out.println(Arrays.toString(nums1));
     }
 
