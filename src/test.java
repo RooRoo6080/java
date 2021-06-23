b5d4a2a83b0db6c3bf62b1f2ca7246ce89bff111
RooRoo6080 <67977174+RooRoo6080@users.noreply.github.com>
2021-06-23 11:30:12 -0700

    Delete test.java

--- src/test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main (String[] args) {
-        ArrayMaker am = new ArrayMaker();
-        for (Integer i : am.arrayMaker(1, 23)) {
-            System.out.print(i + " ");
-        }
-        System.out.println(am.arrayMaker(1, 23)[5]);
-    }
-}
