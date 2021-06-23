e1141e44064855865cb5be1ceb5921b40d7396c4
RooRoo6080 <reuelriju@gmail.com>
2021-06-23 12:33:23 -0700

    first commit

---
+++ src/leetcode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int smallest = Integer.MAX_VALUE;
+        int profit = 0;
+        for (int i : prices) {
+            if (i < smallest) {
+                smallest = i;
+            } else if (i - smallest > profit) {
+                profit = i - smallest;
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        BestTimeToBuyAndSellStock bttbass = new BestTimeToBuyAndSellStock();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(bttbass.maxProfit(prices));
+    }
+}
