c920dfe57de5b44ff606e9f1624608a0057044ee
RooRoo6080 <reuelriju@gmail.com>
2021-06-28 10:46:20 -0700

    first commit

---
+++ src/leetcode/ExcelSheetColumnTitle.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class ExcelSheetColumnTitle {
+    public String convertToTitle(int n) {
+        StringBuilder result = new StringBuilder();
+        while (n > 0) {
+            n--;
+            result.append((char)('A' + n % 26));
+            n /= 26;
+        }
+        result.reverse();
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        ExcelSheetColumnTitle escs = new ExcelSheetColumnTitle();
+        System.out.println(escs.convertToTitle(1000));
+    }
+}
