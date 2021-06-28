5cb758d51655b47f4f4ed867f68b58f6559628dd
RooRoo6080 <reuelriju@gmail.com>
2021-06-28 13:12:47 -0700

    first commit

---
+++ src/leetcode/MajorityElement.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int len = nums.length;
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for (int i : nums) {
+            if (count.containsKey(i)) {
+                count.put(i, count.get(i) + 1);
+                if (count.get(i) > len / 2) return i;
+            } else {
+                count.put(i, 1);
+            }
+        }
+        return nums[0];
+    }
+
+    public static void main(String[] args) {
+        MajorityElement me = new MajorityElement();
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(me.majorityElement(nums));
+    }
+}
