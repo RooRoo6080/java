03678a475cdf51255e1f831765d6449dd83c2a9d
RooRoo6080 <reuelriju@gmail.com>
2021-06-30 13:23:38 -0700

    first commit

---
+++ src/leetcode/CountPrimes.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class CountPrimes {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                count++;
+                for (int j = 2; i*j < n; j++) {
+                    notPrime[i*j] = true;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        CountPrimes cp = new CountPrimes();
+        System.out.println(cp.countPrimes(499979));
+    }
+}
