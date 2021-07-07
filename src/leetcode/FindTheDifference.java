da0c388d577a5ae5afe9aa88ca263878222d2b67
RooRoo6080 <reuelriju@gmail.com>
2021-07-07 13:20:12 -0700

    first commit

---
+++ src/leetcode/FindTheDifference.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class FindTheDifference {
+    public char findTheDifference(String s, String t) {
+        int sNum = 0, tNum = 0;
+        for (int i = 0; i < s.length(); i++) sNum += s.charAt(i);
+        for (int i = 0; i < t.length(); i++) tNum += t.charAt(i);
+        return (char) (tNum - sNum);
+    }
+
+    public static void main(String[] args) {
+        FindTheDifference ftd = new FindTheDifference();
+        String s = "abcd";
+        String t = "acbde";
+        System.out.println(ftd.findTheDifference(s, t));
+    }
+}
