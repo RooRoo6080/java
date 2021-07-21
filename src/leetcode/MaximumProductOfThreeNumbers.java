b36c16eee2ce3a559900a9a5829b23bb0000dbdd
RooRoo6080 <reuelriju@gmail.com>
2021-07-21 11:59:04 -0700

    first commit

---
+++ src/leetcode/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class MaximumProductOfThreeNumbers {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        return Math.max(nums[0] * nums[1] * nums[nums.length - 1], nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3]);
+    }
+
+    public static void main(String[] args) {
+        MaximumProductOfThreeNumbers mpotn = new MaximumProductOfThreeNumbers();
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(mpotn.maximumProduct(nums));
+    }
+}
