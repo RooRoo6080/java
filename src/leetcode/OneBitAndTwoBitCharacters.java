3569d8a508fe595dd7eaa0d33606b3dbfaf32b46
RooRoo6080 <reuelriju@gmail.com>
2021-07-26 12:27:31 -0700

    first commit

---
+++ src/leetcode/OneBitAndTwoBitCharacters.java
@@ -0,0 +1,15 @@
+package leetcode;
+// #717
+public class OneBitAndTwoBitCharacters {
+    public boolean isOneBitCharacter(int[] bits) {
+        int ones = 0;
+        for (int i = bits.length - 2; i >= 0 && bits[i] != 0 ; i--) ones++;
+        return ones % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        OneBitAndTwoBitCharacters obatbc = new OneBitAndTwoBitCharacters();
+        int[] bits = {0, 1, 0};
+        System.out.println(obatbc.isOneBitCharacter(bits));
+    }
+}
---
+++ src/leetcode/ToLowerCase.java
@@ -0,0 +1,12 @@
+package leetcode;
+//709
+public class ToLowerCase {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        ToLowerCase tlc = new ToLowerCase();
+        System.out.println(tlc.toLowerCase("Hello"));
+    }
+}
