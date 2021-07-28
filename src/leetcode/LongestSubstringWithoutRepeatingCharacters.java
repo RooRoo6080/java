8e0391c47f3151869458eaae9678b40bf535c8e8
RooRoo6080 <reuelriju@gmail.com>
2021-07-28 12:36:39 -0700

    first commit

---
+++ src/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        int result = 0;
+        int[] cache = new int[256];
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            j = (cache[s.charAt(i)] > 0) ? Math.max(j, cache[s.charAt(i)]) : j;
+            cache[s.charAt(i)] = i + 1;
+            result = Math.max(result, i - j + 1);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LongestSubstringWithoutRepeatingCharacters lswrc = new LongestSubstringWithoutRepeatingCharacters();
+        System.out.println(lswrc.lengthOfLongestSubstring("afgagethdarg"));
+    }
+}
