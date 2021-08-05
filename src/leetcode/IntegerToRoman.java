a8fe7a584c1ab8c3cb7af7fe8e0b90f0376c8a1c
RooRoo6080 <reuelriju@gmail.com>
2021-08-05 11:23:43 -0700

    second commit - 2d array

--- src/leetcode/IntegerToRoman.java
+++ src/leetcode/IntegerToRoman.java
@@ -2,11 +2,13 @@ package leetcode;
 
 public class IntegerToRoman {
     public String intToRoman(int num) {
-        String[] m = {"", "M", "MM", "MMM"};
-        String[] c = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        String[] x = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        String[] i = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        return m[num/1000] + c[(num%1000)/100] + x[(num%100)/10] + i[num%10];
+        String[][] all = {
+                {"", "M", "MM", "MMM"},
+                {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+                {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+                {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
+        };
+        return all[0][num/1000] + all[1][(num%1000)/100] + all[2][(num%100)/10] + all[3][num%10];
     }
 
     public static void main(String[] args) {
