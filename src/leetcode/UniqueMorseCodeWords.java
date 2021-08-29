7c3c880155d8a25c0c45fc22d4348ca7e16121f4
RooRoo6080 <reuelriju@gmail.com>
2021-08-28 22:35:22 -0700

    first commit

---
+++ src/leetcode/UniqueMorseCodeWords.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.HashSet;
+
+public class UniqueMorseCodeWords {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] letters = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        HashSet<String> combos = new HashSet<>();
+        for (String s : words) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                sb.append(letters[s.charAt(i) - 97]);
+            }
+            combos.add(sb.toString());
+        }
+        return combos.size();
+    }
+
+    public static void main(String[] args) {
+        UniqueMorseCodeWords umcw = new UniqueMorseCodeWords();
+        String[] words = {"gin","zen","gig","msg"};
+        System.out.println(umcw.uniqueMorseRepresentations(words));
+    }
+}
