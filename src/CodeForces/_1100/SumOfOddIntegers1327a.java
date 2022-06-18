88184c52953beaaaa12db8f8f336d002d7f10faf
RooRoo6080 <reuelriju@gmail.com>
2022-06-18 12:27:42 -0700

    first commit

--- src/CodeForces/_1100/SumOfOddIntegers1327a.java
+++ src/CodeForces/_1100/SumOfOddIntegers1327a.java
@@ -8,6 +8,10 @@ public class SumOfOddIntegers1327a {
         int t = scanner.nextInt();
         for (int i = 0; i < t; i++) {
             int n = scanner.nextInt(), k = scanner.nextInt(), sub = 1;
+            if (k > n) {
+                System.out.println("NO");
+                continue;
+            }
             for (int j = 0; j < k - 1; j++) {
                 n -= sub;
                 sub += 2;
