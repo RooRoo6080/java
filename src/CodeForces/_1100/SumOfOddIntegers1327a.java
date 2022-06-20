fa29580d5b670328a329540de9a059cfb57c8dec
RooRoo6080 <reuelriju@gmail.com>
2022-06-20 12:21:21 -0700

    first commit

--- src/CodeForces/_1100/SumOfOddIntegers1327a.java
+++ src/CodeForces/_1100/SumOfOddIntegers1327a.java
@@ -1,8 +1,11 @@
 package CodeForces._1100;
 
+// NO SOLUTION WORKING (problem error?)
+
 import java.util.Scanner;
 
 public class SumOfOddIntegers1327a {
+    /* v1 - failed testcase 3-3166
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
@@ -20,4 +23,15 @@ public class SumOfOddIntegers1327a {
             else System.out.println("YES");
         }
     }
+    v2 - failed testcase 3-27556
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = scanner.nextInt(), k = scanner.nextInt();
+            if (k * k <= n && (n + k) % 2 == 0) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+     */
 }
