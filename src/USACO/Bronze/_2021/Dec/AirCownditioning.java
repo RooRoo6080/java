727417f2a6d9fe2fcefdff4bb7d50425ffb6f736
RooRoo6080 <reuelriju@gmail.com>
2022-06-30 12:46:58 -0700

    first commit

--- src/USACO/Bronze/_2021/Dec/AirCownditioning.java
+++ src/USACO/Bronze/_2021/Dec/AirCownditioning.java
@@ -1,7 +1,5 @@
 package USACO.Bronze._2021.Dec;
 
-// WIP
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -11,9 +9,33 @@ public class AirCownditioning {
         for (int r = 1; r < 11; r++) {
             long startTime = System.currentTimeMillis();
             Scanner scanner = new Scanner(new File("src/USACO/Bronze/_2021/Dec/prob2_bronze_dec21/" + r + ".in"));
-            int n = scanner.nextInt(), count = 0;
-            char[] s = scanner.nextLine().toCharArray();
-            char[] p = scanner.nextLine().toCharArray();
+            int n = scanner.nextInt(), count = 0, f = 0;
+            int[] o = new int[n];
+            for (int i = 0; i < n; i++) o[i] = scanner.nextInt();
+            for (int i = 0; i < n; i++) o[i] -= scanner.nextInt();
+            for (int i = 0; i < n; i++) if (o[i] == 0) f++;
+            while (f < n) {
+                for (int i = 0; i < n; i++) {
+                    if (o[i] != 0) {
+                        count++;
+                        if (i > 0) {
+                            if ((o[i - 1] > 0 && o[i] > 0) || (o[i - 1] < 0 && o[i] < 0)) {
+                                count--;
+                            }
+                        }
+                    }
+                }
+                for (int j = 0; j < n; j++) {
+                    if (o[j] > 0) {
+                        o[j]--;
+                        if (o[j] == 0) f++;
+                    }
+                    else if (o[j] < 0) {
+                        o[j]++;
+                        if (o[j] == 0) f++;
+                    }
+                }
+            }
             System.out.println(r + ": " + count);
             Scanner check = new Scanner(new File("src/USACO/Bronze/_2021/Dec/prob2_bronze_dec21/" + r + ".out"));
             if (count == check.nextLong()) System.out.println("CORRECT - " + (System.currentTimeMillis() - startTime) + "ms \n");
