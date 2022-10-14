4cf8f85e052c93d3155d14ff833e8a7d50d5e2e9
RooRoo6080 <reuelriju@gmail.com>
2022-10-14 12:38:50 -0700

    first commit

---
+++ src/CodeForces/_1200/Divisibility630j.java
@@ -0,0 +1,10 @@
+package CodeForces._1200;
+
+import java.util.Scanner;
+
+public class Divisibility630j {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLong() / 2520);
+    }
+}
