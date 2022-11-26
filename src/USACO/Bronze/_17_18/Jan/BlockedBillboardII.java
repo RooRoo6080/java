0e03d9b8b4f85f2927911e2671800f094cf31014
RooRoo6080 <reuelriju@gmail.com>
2022-11-25 20:13:03 -0800

    first commit

--- src/USACO/Bronze/_17_18/Jan/BlockedBillboardII.java
+++ src/USACO/Bronze/_17_18/Jan/BlockedBillboardII.java
@@ -1,7 +1,11 @@
-import java.util.*;
-import java.io.*;
+package USACO.Bronze._17_18.Jan;
 
-public class Main {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BlockedBillboardII {
     public static boolean covered(int x, int y, int x1, int y1, int x2, int y2) {
         return x >= x1 && x <= x2 && y >= y1 && y <= y2;
     }
@@ -20,7 +24,7 @@ public class Main {
         if(c < 2) writer.write(String.valueOf(i));
         else if (c == 4) writer.write(String.valueOf(4));
         else { writer.write(i - (Math.min(lb[2], cfb[2]) - Math.max(lb[0], cfb[0]))
-                * (Math.min(lb[3], cfb[3]) - Math.max(lb[1], cfb[1]))); }
+                    * (Math.min(lb[3], cfb[3]) - Math.max(lb[1], cfb[1]))); }
         writer.close();
     }
 }
--- src/USACO/SUBMISSION_TEMPLATE.java
+++ src/USACO/SUBMISSION_TEMPLATE.java
@@ -5,12 +5,13 @@ import java.util.*;
 
 public class SUBMISSION_TEMPLATE {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(new File("test.in"));
-        FileWriter writer = new FileWriter("test.out");
+        //Scanner scanner = new Scanner(new File("test.in"));
+        Scanner scanner = new Scanner(System.in);
+        //FileWriter writer = new FileWriter("test.out");
         int n = scanner.nextInt();
 
 
-        writer.write(String.valueOf(1));
-        writer.close();
+        //writer.write(String.valueOf(1));
+        //writer.close();
     }
 }
